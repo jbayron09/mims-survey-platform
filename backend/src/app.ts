4bf2f9539615601275e4758ab485878d190827da
jbayron09 <j.bayron.b@gmail.com>
2025-07-09 01:14:24 -0500

    Add Swagger documentation for Survey API endpoints

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from './config/swagger.json'
 import surveyRouter from './modules/survey/survey.router'
 import responseRouter from './modules/response/response.router'
 
@@ -11,6 +13,9 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+//documentation
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 // Rutas de API
 app.use('/api/surveys', surveyRouter)
